29c025ee1ac206a07ae75a82dd2f7ed88bc811ad
mars <amalush1@unca.edu>
2023-03-13 12:13:00 -0400

    lecture 15

---
+++ lectures/lecture15/01-style-property-demo/main.js
@@ -0,0 +1,40 @@
+
+function makeRed() {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = '#ec5747';
+  
+};
+
+// function makeRed() { 
+//     // your code here...
+//     console.log('Change background to red');
+//     document.querySelector('#section1').style.backgroundColor = '#ec5747';
+
+// document.querySelector('body').color = '#FFFFFF';
+
+
+
+function makeBlue() {
+    // your code here...
+    console.log('Change background to blue');
+    document.querySelector('body').style.backgroundColor='#7291a5';
+
+};
+
+function makePink() {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('body').style.backgroundColor='pink';
+};
+
+function makeOrange() {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('body').style.backgroundColor='#f89e28';
+};
+
+function clear() {
+    console.log('Clear all section (no bg, font color white)');
+    document.querySelector('#section1').style.bacgkroundColor = "white";
+}
---
+++ lectures/lecture15/02-attribute-demo/main.js
@@ -0,0 +1,32 @@
+/* 
+cat:  https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg
+dog:  https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg
+bird: https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg
+fish: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3H7Yb30u3WF_hq9nJR5qV8kecf0mUhJoDnA&usqp=CAU
+*/
+
+
+function showCat() {
+    // your code here...
+    console.log('update the image to show a cat!');
+    document.querySelector('#image-demo').src='https://cdn.shopify.com/s/files/1/0331/9182/1447/articles/10_fascinating_facts_about_black_cats-3_1200x1200.jpg?v=1667409596'
+};
+
+function showDog() {
+    // your code here...
+    console.log('update the image to show a dog!');
+    document.querySelector('#image-demo').src='https://www.scotsman.com/webimg/b25lY21zOjdlNGNiYTJjLWRhMTAtNGJhYS04N2U2LWRiM2U1MjQ0ZDkzYjoxM2U0Y2FkMi1jYzc0LTQ0ZTYtYjc5My02ZDg5Y2MwOGMzZjY=.jpg?width=990&quality=65&enable=upscale'
+};
+
+function showBird() {
+    // your code here...
+    console.log('update the image to show a bird!');
+    document.querySelector('#image-demo').src='https://media.audubon.org/tufted_titmouse._photo_deborah_bifulco_great_backyard_bird_count.jpg'
+};
+
+function showFish() {
+    // your code here...
+    console.log('update the image to show a fish!');
+    document.querySelector('#image-demo').src='https://i.natgeofe.com/n/d3ef5a05-02af-4832-9fc4-b1a6a3b4fde9/thumbnail-alligator-gar-nationalgeographic_1320259_4x3.jpg'
+};
+
