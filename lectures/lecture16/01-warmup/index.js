0e7f4f2998b057506134a890df393c74866096d8
mars <amalush1@unca.edu>
2023-03-17 16:05:32 -0400

    quiz 2

---
+++ lectures/lecture16/01-warmup/index.js
@@ -0,0 +1,28 @@
+function toggleFall() {
+    /*
+        Your task: target the body element and change it's class to "fall"
+    */
+   document.querySelector('body').classname='fall';
+
+}
+
+function toggleWinter() {
+    /*
+        Your task: target the body element and change it's class to "winter"
+    */
+        document.querySelector('body').classname='winter';
+     }
+
+function toggleSpring() {
+    /*
+        Your task: target the body element and change it's class to "spring"
+    */
+        document.querySelector('body').classname='spring';
+}
+
+function toggleUnca() {
+    /*
+        Your task: target the body element and change it's class to "unca"
+    */
+        document.querySelector('body').classname='Unca';
+}
